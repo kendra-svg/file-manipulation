755903168b635b52392b85debb838cbb36dc0b0c
kendra-svg <ksanchov@ucenfotec.ac.cr>
2025-04-19 08:56:08 -0600

    Add file reader and counter details implementation

---
+++ FileManipulationHandler/Program.cs
@@ -0,0 +1,8 @@
+﻿var fileWriter = new Writer();
+var fileReader = new Reader();
+var fileDetailsCounter = new Counter();
+
+fileReader.ReadLinesFromFileAndPrint();
+fileDetailsCounter.CountLines();
+fileDetailsCounter.CountWords();
+fileDetailsCounter.CountChars();
