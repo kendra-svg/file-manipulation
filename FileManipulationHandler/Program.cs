6ce3fafb1cd2f0a48db5da1c5ab763efe4fd660a
kendra-svg <ksanchov@ucenfotec.ac.cr>
2025-04-20 09:15:36 -0600

    Add filter log level keyword functional implementation

--- FileManipulationHandler/Program.cs
+++ FileManipulationHandler/Program.cs
@@ -1,10 +1,15 @@
-﻿var fileWriter = new Writer();
+﻿using FileManipulationHandler.Medium;
+
+var fileWriter = new Writer();
 var fileReader = new Reader();
 var fileDetailsCounter = new Counter();
+var keywordFilter = new KeywordFilter();
 
 //fileReader.ReadLinesFromFileAndPrint();
 //fileDetailsCounter.CountLines();
 //fileDetailsCounter.CountWords();
 //fileDetailsCounter.CountChars();
 
-fileWriter.CreateFile();
+//fileWriter.CreateFile();
+
+keywordFilter.Filter();
